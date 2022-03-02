84001e2f0d428cee9a689c8e0b3344dfd941ed8f
Phil Calvin <phillip.calvin@eleostech.com>
2022-03-02 08:42:39 -0500

    Add draft banner

--- OpenCabProvider/build-docs.sh
+++ OpenCabProvider/build-docs.sh
@@ -2,5 +2,20 @@
 
 set -ex
 
+# Unless this is a production website build, add a draft banner
+
+if [ "$VERCEL_ENV" != "production" ]; then
+    cat << EOF >> ../website/main.css
+.fixedNav::before {
+    text-align: center;
+    content: "Draft Proposal — For Review Only";
+    background-color: hsl(45, 100%, 55%);
+    display: block;
+    font-size: 150%;
+    padding: 5px 0;
+}
+EOF
+fi
+
 ./gradlew javadoc
 cp -R ../website/images app/build/docs/javadoc
