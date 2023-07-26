d7aa8c32c0ea63cff7b432163c4bb26ec7d53b57
Phil Calvin <phillip.calvin@eleostech.com>
2023-07-26 19:22:29 -0400

    Exercise more clock types in example provider data

--- OpenCabProvider/app/src/main/java/com/eleostech/exampleprovider/HOSUtil.java
+++ OpenCabProvider/app/src/main/java/com/eleostech/exampleprovider/HOSUtil.java
@@ -14,26 +14,31 @@ public class HOSUtil {
     private static final String HOS_DURATION_CLOCK_LABEL = "HOS Test Time";
 
     public static HOSContract.HOSStatusV2 getHOSStatusV2(Context context, boolean isTeamDriver) {
+        long currentMillis = System.currentTimeMillis();
+        Date currentMillisDate = new Date(currentMillis);
+
         String username = isTeamDriver ? "OPENCAB_TEAM_DRIVER" : Preferences.getUsername(context);
         String duty = null;
         Date date = new Date();
         String label = null;
+        boolean useDurationSeconds = false;
         boolean limit = false;
         switch (Preferences.getDutyStatus(context)) {
             case "d":
                 duty = "D";
-                date = addHoursToDate(new Date(), 12);
+                date = addHoursToDate(currentMillisDate, 11);
                 label = "Drive Time Remaining";
                 limit = true;
                 break;
             case "on":
                 duty = "ON";
-                date = addHoursToDate(new Date(), 8);
+                date = addHoursToDate(currentMillisDate, 7);
                 label = "On Duty Time Remaining";
+                useDurationSeconds = true;
                 break;
             case "off":
                 duty = "OFF";
-                date = new Date();
+                date = addHoursToDate(currentMillisDate, 9);
                 label = "Rest Time Remaining";
                 break;
         }
@@ -50,6 +55,7 @@ public class HOSUtil {
 
         SimpleDateFormat format = new SimpleDateFormat(HOSProvider.DATE_FORMAT);
         item2.setValue(format.format(date));
+        item2.setImportant(true);
         clocks.add(item2);
 
         HOSContract.ClockV2 item3 = new HOSContract.ClockV2();
@@ -61,16 +67,26 @@ public class HOSUtil {
         HOSContract.ClockV2 item4 = new HOSContract.ClockV2();
         item4.setLabel("Time since Rest");
         item4.setValueType(HOSContract.ClockV2.ValueType.COUNTUP);
-        item4.setValue(format.format(new Date()));
+        Long time = new Date().getTime();
+        Date midnight = new Date(time - time % (24 * 60 * 60 * 1000));
+        item4.setValue(format.format(midnight));
         clocks.add(item4);
 
         HOSContract.ClockV2 item5 = new HOSContract.ClockV2();
         item5.setLabel(HOS_DURATION_CLOCK_LABEL);
         item5.setValueType(HOSContract.ClockV2.ValueType.STRING);
-        item5.setValue("0:04");
-        item5.setDurationSeconds(240.0);
-        item5.setLimitsDrivingRange(true);
+        long dateMillis = (date.getTime() - new Date().getTime()) / 1000;
+        item5.setValue(String.format("%02d:%02d", dateMillis / 60 / 60, dateMillis / 60 % 60));
+        item5.setDurationSeconds(new Double(dateMillis));
+        item5.setLimitsDrivingRange(useDurationSeconds);
         clocks.add(item5);
+
+        HOSContract.ClockV2 item6 = new HOSContract.ClockV2();
+        item6.setLabel("Today's Date");
+        item6.setValueType(HOSContract.ClockV2.ValueType.DATE);
+        String strDate = format.format(new Date());
+        item6.setValue(strDate);
+        clocks.add(item6);
 
         HOSContract.HOSStatusV2 hosStatusV2 = new HOSContract.HOSStatusV2();
         hosStatusV2.setClocks(clocks);
@@ -82,6 +98,9 @@ public class HOSUtil {
     }
 
     public static HOSContract.HOSStatus getHOSStatus(Context context, boolean isTeamDriver) {
+        long currentMillis = System.currentTimeMillis();
+        Date currentMillisDate = new Date(currentMillis);
+
         String username = isTeamDriver ? "OPENCAB_TEAM_DRIVER" : Preferences.getUsername(context);
         String duty = null;
         Date date = new Date();
@@ -91,18 +110,18 @@ public class HOSUtil {
             switch (Preferences.getDutyStatus(context)) {
                 case "d":
                     duty = "D";
-                    date = addHoursToDate(new Date(), 12);
+                    date = addHoursToDate(currentMillisDate, 11);
                     label = "Drive Time Remaining";
                     limit = true;
                     break;
                 case "on":
                     duty = "ON";
-                    date = addHoursToDate(new Date(), 8);
+                    date = addHoursToDate(currentMillisDate, 7);
                     label = "On Duty Time Remaining";
                     break;
                 case "off":
                     duty = "OFF";
-                    date = new Date();
+                    date = addHoursToDate(currentMillisDate, 9);
                     label = "Rest Time Remaining";
                     break;
             }
@@ -131,20 +150,31 @@ public class HOSUtil {
         HOSContract.Clock item4 = new HOSContract.Clock();
         item4.setLabel("Time since Rest");
         item4.setValueType(HOSContract.Clock.ValueType.COUNTUP);
-        item4.setValue(format.format(new Date()));
+        Long time = new Date().getTime();
+        Date midnight = new Date(time - time % (24 * 60 * 60 * 1000));
+        item4.setValue(format.format(midnight));
         clocks.add(item4);
 
-        HOSContract.HOSStatus hosStatusV2 = new HOSContract.HOSStatus();
-        hosStatusV2.setClocks(clocks);
+        HOSContract.Clock item5 = new HOSContract.Clock();
+        item5.setLabel("Today's Date");
+        item5.setValueType(HOSContract.Clock.ValueType.DATE);
+        String strDate = format.format(new Date());
+        item5.setValue(strDate);
+        clocks.add(item5);
+
+        HOSContract.HOSStatus hosStatus = new HOSContract.HOSStatus();
+        hosStatus.setClocks(clocks);
         if (Preferences.isManageAction(context)) {
-            hosStatusV2.setManageAction("hos://com.eleostech.opencabprovider/hos");
+            hosStatus.setManageAction("hos://com.eleostech.opencabprovider/hos");
         }
-        return hosStatusV2;
+        return hosStatus;
     }
 
     private static Date addHoursToDate(Date date, int hours) {
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(date);
+        int minutes = calendar.get(Calendar.MINUTE);
+        calendar.add(Calendar.MINUTE, 60 - minutes);
         calendar.add(Calendar.HOUR_OF_DAY, hours);
         return calendar.getTime();
     }
